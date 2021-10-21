722ca576aceadd44b20b7782b8c1eba10366206d
alejandro <alejandroweil13@gmail.com>
2021-10-20 22:25:47 -0400

    Test calculator class with attr_reader

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,5 +1,8 @@
 class Calculator
   
+  attr_reader :grocery_list  
+
+
   def initialize(grocery_list)
     @grocery_list = grocery_list
   end
@@ -11,12 +14,19 @@ class Calculator
 
 end
 
-puts "         Items for Purchase"
-puts "Item     Unit Price      Sales price"
-puts "-------------------------------------"
-puts "Milk      $3.97           2 for $5.00"
-puts "Bread     $2.17           3 for $6.00"
-puts "Banana    $0.99           "
-puts "Apple     $0.89            "
-puts ""
-puts "Please enter all of the items in your list separated by a comma"
+def intro
+  puts "         Items for Purchase"
+  puts "Item     Unit Price      Sales price"
+  puts "-------------------------------------"
+  puts "Milk      $3.97           2 for $5.00"
+  puts "Bread     $2.17           3 for $6.00"
+  puts "Banana    $0.99           "
+  puts "Apple     $0.89            "
+  puts ""
+  puts "Please enter all of the items in your list separated by a comma with no space"
+end
+
+intro
+list = gets
+
+
