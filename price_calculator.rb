42132c4a7820bbba58b73f21febcadc6e16244a7
alejandro <alejandroweil13@gmail.com>
2021-10-20 22:14:40 -0400

    Create intro with item list and sales prices

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,7 +1,22 @@
 class Calculator
   
-  
-  def initialize(user_input)
-    @user_input = user_input
+  def initialize(grocery_list)
+    @grocery_list = grocery_list
   end
+
+  def calculate_price
+
+
+  end
+
 end
+
+puts "         Items for Purchase"
+puts "Item     Unit Price      Sales price"
+puts "-------------------------------------"
+puts "Milk      $3.97           2 for $5.00"
+puts "Bread     $2.17           3 for $6.00"
+puts "Banana    $0.99           "
+puts "Apple     $0.89            "
+puts ""
+puts "Please enter all of the items in your list separated by a comma"
